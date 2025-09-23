38e6d83535c355a1d395f5bac0ced39585c34230
hfelices92 <hfelicesg@fp.insjoaquimmir.cat>
2025-09-23 21:13:18 +0200

    projects CRUD

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,61 @@
+import type { Request, Response } from "express";
+import colors from "colors";
+import Project from "../models/Project";
+
+export class ProjectController {
+  static createProject = async (req: Request, res: Response) => {
+    const project = new Project(req.body);
+    try {
+      await project.save();
+      res.send("Project created successfully");
+    } catch (error) {
+      console.log(colors.red.bold(error));
+    }
+  };
+  static getAllProjects = async (req: Request, res: Response) => {
+    try {
+      const projects = await Project.find({});
+      res.json(projects);
+    } catch (error) {
+      console.log(colors.red.bold(error));
+    }
+  };
+
+  static getProjectById = async (req: Request, res: Response) => {
+    try {
+      const project = await Project.findById(req.params.id);
+      if (!project) {
+        return res.status(404).json({ error: "Project not found" });
+      }
+      res.json(project);
+    } catch (error) {
+      console.log(colors.red.bold(error));
+    }
+  };
+
+  static updateProject = async (req: Request, res: Response) => {
+    try {
+      const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (!project) {
+        return res.status(404).json({ error: "Project not found" });
+      }
+      res.json(project);
+    } catch (error) {
+      console.log(colors.red.bold(error));
+    }
+  };
+
+  static deleteProject = async (req: Request, res: Response) => {
+    try {
+      const project = await Project.findByIdAndDelete(req.params.id);
+      if (!project) {
+        return res.status(404).json({ error: "Project not found" });
+      }
+      res.json({ message: "Project deleted successfully" });
+    } catch (error) {
+      console.log(colors.red.bold(error));
+    }
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./server";
 import colors from "colors";
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(colors.bgGreen.bold(` Server running on port ${PORT} `));
